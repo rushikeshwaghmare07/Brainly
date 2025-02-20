8e6c3dbe33b17ad8e4a0b8c9c3b58eedb118178a
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-20 23:25:39 +0530

    Add user schema

---
+++ src/models/user.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document, Schema, Model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const UserModel: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
+
+export default UserModel;
