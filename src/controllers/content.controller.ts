e028cda9cc001c876612cccfeada5f6227f73414
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-21 19:15:32 +0530

    Implement delete content functionality

--- src/controllers/content.controller.ts
+++ src/controllers/content.controller.ts
@@ -39,13 +39,10 @@ export const addContent = async (
   }
 };
 
-export const getContent = async (
-  req: Request,
-  res: Response
-) => {
+export const getContent = async (req: Request, res: Response) => {
   try {
     const userId = req.userId;
-    
+
     const content = await ContentModel.find({ userId }).populate({
       path: "userId",
       select: "username",
@@ -53,15 +50,15 @@ export const getContent = async (
     if (!content) {
       res.status(400).json({
         success: false,
-        message: "No Content Available."
-      })
+        message: "No Content Available.",
+      });
     }
 
     res.status(200).json({
       success: false,
       message: "Content retrieved successfully.",
-      content: content
-    })
+      content: content,
+    });
   } catch (error) {
     console.error(
       `Error in getContent controller: ${(error as Error).message}`
@@ -71,4 +68,43 @@ export const getContent = async (
       message: "Internal server error. Please try again later.",
     });
   }
-}
+};
+
+export const deleteContent = async (req: Request, res: Response): Promise<void>  => {
+  try {
+    const { contentId } = req.params;
+
+    if (!contentId) {
+      res.status(400).json({
+        success: false,
+        message: "Content ID is required.",
+      });
+      return;
+    }
+
+    const content = await ContentModel.findOne({ _id: contentId, userId: req.userId });
+
+    if (!content) {
+      res.status(404).json({
+        success: false,
+        message: "Content not found or unauthorized.",
+      });
+      return;
+    }
+
+    await ContentModel.findByIdAndDelete(contentId);
+
+    res.status(200).json({
+      success: true,
+      message: "Content deleted successfully.",
+    });
+  } catch (error) {
+    console.error(
+      `Error in deleteContent controller: ${(error as Error).message}`
+    );
+    res.status(500).json({
+      success: false,
+      message: "Internal server error. Please try again later.",
+    });
+  }
+};
--- src/routes/content.routes.ts
+++ src/routes/content.routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { addContent, getContent } from "../controllers/content.controller";
+import {
+  addContent,
+  deleteContent,
+  getContent,
+} from "../controllers/content.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.post("/", authMiddleware , addContent);
-router.get("/", authMiddleware , getContent);
+router.post("/", authMiddleware, addContent);
+router.get("/", authMiddleware, getContent);
+router.delete("/:contentId", authMiddleware, deleteContent);
 
 export default router;
