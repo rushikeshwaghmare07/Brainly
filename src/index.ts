4fa6b2d8a5428e47ef54c264a74623e93aee62f4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-21 13:43:37 +0530

    Update middleware and route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./db/index.js";
+import userRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/users", userRoutes);
 
 connectDB()
   .then(() => {
