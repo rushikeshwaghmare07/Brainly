c450a53db1a089c1f5258eb374e530cb55316152
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-26 01:17:30 +0530

    Implement get share link data functionality

--- backend/src/controllers/share.controller.ts
+++ backend/src/controllers/share.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import LinkModel from "../models/Link.model";
 import { random } from "../utils/randomHash";
+import ContentModel from "../models/content.model";
+import UserModel from "../models/user.model";
 
 export const share = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -43,4 +45,47 @@ export const share = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+export const shareLink = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const hash = req.params.shareLink;
+    if (!hash) {
+      res.status(400).json({
+        success: false,
+        message: "Hash is required.",
+      });
+    }
 
+    const link = await LinkModel.findOne({ hash });
+    if (!link) {
+      res.status(404).json({
+        success: false,
+        message: "Link not found.",
+      });
+      return;
+    }
+
+    const content = await ContentModel.findOne({ userId: link.userId });
+    if (!content) {
+      res.status(404).json({
+        success: false,
+        message: "Content not found.",
+      });
+      return;
+    }
+
+    const user = await UserModel.findOne({ _id: link.userId });
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully shared the link.",
+      username: user?.username,
+      content: content,
+    });
+  } catch (error) {
+    console.log("Error in shareLink controller:", (error as Error).message);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong while sharing the link.",
+    });
+  }
+};
--- backend/src/routes/share.routes.ts
+++ backend/src/routes/share.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { share } from "../controllers/share.controller.js";
+import { share, shareLink } from "../controllers/share.controller.js";
 
 const router = express.Router();
 
 router.post("/share", authMiddleware, share);
+router.get("/:shareLink", shareLink);
 
 export default router;
